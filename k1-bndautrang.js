6721effaa68c3b4e77f273e02be471e18af1ae8c
lehoantrungthien <lethanhtrungdhnl4@gmail.com>
2018-03-13 12:17:05 +0700

    Update k1-bndautrang.js

--- k1-bndautrang.js
+++ k1-bndautrang.js
@@ -117,7 +117,7 @@ jQuery(window).load(function() {
 			jQuery(divpopup).attr('id', 'myModal');
 			jQuery(divpopup).attr('class', 'linhnguyen-modal');
 			jQuery( "body" ).append(divpopup);
-			jQuery("#myModal").html("<a href='https://drive.google.com/file/d/0B-xKAFheP7D6OXk0ZklKZ24wYVE/view' target='_blank'><img src='https://drive.google.com/file/d/1bBMuEBPDnAH8tgiGCrzbbw1KGM8e89BH/view' width='500px'/><br/>Nhà thầu cơ điện </a> tiêu chuẩn Nhật Bản. <h2><a class='close-linhnguyen-modal'>X</a></h2>");
+			jQuery("#myModal").html("<a href='http://www.nhathaucodienvn.com/' target='_blank'><img src='https://drive.google.com/file/d/1bBMuEBPDnAH8tgiGCrzbbw1KGM8e89BH/view' width='500px'/><br/>Nhà thầu cơ điện </a> tiêu chuẩn Nhật Bản. <h2><a class='close-linhnguyen-modal'>X</a></h2>");
 			jQuery('#myModal').linhnguyen(jQuery('#myModal').data());
 		}
 });
